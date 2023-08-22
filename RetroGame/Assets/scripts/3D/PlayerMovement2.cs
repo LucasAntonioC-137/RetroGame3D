737f778f2e245da665fb08c006ad91c236bd1b00
Lucas Antonio <lucas.antonio27@outlook.com>
2023-08-21 21:43:30 -0300

    Reparos e Boost

--- RetroGame/Assets/scripts/3D/PlayerMovement2.cs
+++ RetroGame/Assets/scripts/3D/PlayerMovement2.cs
@@ -7,12 +7,15 @@ using UnityEngine.Rendering.PostProcessing;
 public class PlayerMovement2 : MonoBehaviour
 {
     [Header("Ship parameters")]
+    [ Space ]
     public float xySpeed = 18;
     public float lookSpeed = 340f;
     public float forwardSpeed = 6;
     public float turnSpeed = 60f;
     public float maxAngle = 50.0f;
+    public float rotationAmount = 75f;
     //public CinemachineDollyCart dolly;
+    [ Space ]
     [Header("Others")]
     public Transform cameraParent;
     public int invert = -1;
@@ -20,13 +23,26 @@ public class PlayerMovement2 : MonoBehaviour
     public Transform aimTarget;
     public CinemachineDollyCart dolly;
     private float originalFov;
+
+    [Space]
+    [Header("Boost")]
+
+    public float boostAmount = 100f; // Quantidade inicial de boost
+    public float boostCost = 10f; // Custo por uso do boost
+    public float boostRechargeRate = 5f; // Taxa de recarga do boost
+
+    private float rotationSpeed = 60f;
+    private float maxRotation = 60f;
+    private float currentRotation = 0f;
+    private float inputDelay = 1f;
+    private float lastInputTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
         playerModel = transform.GetChild(0);
         SetSpeed(forwardSpeed);
         originalFov = Camera.main.fieldOfView;
-        Debug.Log(originalFov.ToString());
+        //Debug.Log(originalFov.ToString());
     }
     // Update is called once per frame
     void Update()
@@ -36,7 +52,14 @@ public class PlayerMovement2 : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, invert * vertical, 0);
         LocalMove(horizontal, vertical, xySpeed);
         RotationLookPath(horizontal, vertical, lookSpeed);
-        HorizontalLean(playerModel, horizontal, 80, .1f);
+        HorizontalLean(playerModel, horizontal, 50, .1f);
+        // Recarregar o boost
+        if (boostAmount < 100f)
+        {
+            boostAmount += boostRechargeRate * Time.deltaTime;
+            if (boostAmount > 100f)
+                boostAmount = 100f;
+        }
         if (Input.GetButtonDown("Fire3"))
             Boost(true);
 
@@ -48,6 +71,22 @@ public class PlayerMovement2 : MonoBehaviour
 
         if (Input.GetButtonUp("Fire4"))
             Break(false);
+
+        if (Time.time - lastInputTime > inputDelay)
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                currentRotation -= rotationSpeed * Time.deltaTime;
+                lastInputTime = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                currentRotation += rotationSpeed * Time.deltaTime;
+                lastInputTime = Time.time;
+            }
+        }
+        currentRotation = Mathf.Clamp(currentRotation, -maxRotation, maxRotation);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, currentRotation);
 
     }
 
@@ -122,6 +161,8 @@ public class PlayerMovement2 : MonoBehaviour
         {
             Camera.main.fieldOfView = originalFov;
         }
+        if (state && boostAmount >= boostCost)
+        {
         float origFov = state ? 50 : 60;
         float endFov = state ? 60 : 60;
         float origChrom = state ? 0 : 1;
@@ -136,9 +177,9 @@ public class PlayerMovement2 : MonoBehaviour
         DOVirtual.Float(origDistortion, endDistorton, .5f, DistortionAmount);
         DOVirtual.Float(dolly.m_Speed, speed, .15f, SetSpeed);
         SetCameraZoom(zoom, .4f);
-
-        // Reset camera zoom to 0 when boost effect ends
-        if (!state)
+        boostAmount -= boostCost;
+        }
+        else if (!state)
         {
             SetCameraZoom(0, .4f);
         }
