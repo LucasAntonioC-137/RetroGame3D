245d8bccbf87178c1fd0ddc9b4d7420b1a5ca763
Arthur_Martins <arthurhavoc@gmail.com>
2024-06-22 17:46:31 -0300

    problema na contagem de rounds resolvido
    agora falta somente finalizar a luta corretamente, há um problema onde ao ser derrotado(qualquer dos dois) o jogo trava em uma contagem de hits infinita.

--- RetroGame/Assets/scripts/2D/Level03/General/RoundCount.cs
+++ RetroGame/Assets/scripts/2D/Level03/General/RoundCount.cs
@@ -14,7 +14,6 @@ public class RoundCount : MonoBehaviour
     private TimeCounter timeCount;
 
     //
-    private BossMovimentation bossMov;
     private PlayerWalk playerWalk;
     private PlayerCombo playerCombo;
     //
@@ -42,39 +41,54 @@ public class RoundCount : MonoBehaviour
     {
         if (timeCount.timer <= 0)
         {
-            GettingRoundResults();
+            float resultsTime = 4f;
             RoundCheckByTime();
+
+            for (float i = 0; i < resultsTime; i += Time.deltaTime)
+            {
+                playerWalk.gameObject.SetActive(false);
+                playerCombo.gameObject.SetActive(false);
+            }
+            playerWalk.gameObject.SetActive(true);
+            playerCombo.gameObject.SetActive(true);
+
+            timeCount = GameObject.Find("Main Camera").GetComponent<TimeCounter>();
+            timeCount.timer += 61f;
         }
 
         if(playerData.life <= 0 || bossData.life <= 0)
         {
-            RoundCheck();
+            GettingRoundResults();
+            if (RoundSingleton.Instance.FinalRound())
+            {
+                playerWalk.gameObject.SetActive(false);
+                playerCombo.gameObject.SetActive(false);
+                
+                //do boss aqui
+            }
+            return;
         }
     }
 
     private void GettingRoundResults()
     {
         float resultsTime = 4f;
-        bool timeIsOver;
+
+        RoundCheck();
+
         //desativar os controles do player e do chefe aqui
-        for(int i = 0; i < resultsTime; i++)
+
+        for (float i = 0; i < resultsTime; i += Time.deltaTime)
         {
             playerWalk.gameObject.SetActive(false);
             playerCombo.gameObject.SetActive(false);
-            timeIsOver = false;
-
-            if(i >= resultsTime)
-            { timeIsOver = true; }
-
-            if (timeIsOver == true)
-            {
-                playerWalk.gameObject.SetActive(true);
-                playerCombo.gameObject.SetActive(true);
-
-                //bossMov aqui
-            }
         }
-
+        playerWalk.gameObject.SetActive(true);
+        playerCombo.gameObject.SetActive(true);
+        
+        timeCount = GameObject.Find("Main Camera").GetComponent<TimeCounter>();
+        timeCount.timer += 61f;
+        //bossMov aqui
     }
 
     private void RoundCheckByTime()
@@ -88,41 +102,36 @@ public class RoundCount : MonoBehaviour
         if (playerData.life < bossData.life)
         {
             lifeIcon[0].sprite = hollow;
-            RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Right);
+            
             //lifeIcon[0].enabled = false;
             //ícone posicionado mais a esquerda, o índice 1 será a última "vida" do jogador e
             //os índices 2 e 3 servirão da mesma forma para o chefe, sendo o índice 2 sua última vida)
-            if ((playerData.life < bossData.life) && 
-                RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Right) > RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Left))// round1Ended == true)
+            if (RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Right) > RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Left))// round1Ended == true)
             {
                 lifeIcon[1].sprite = hollow;
                 RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Right);
+                return; //por enquanto!!!
                 //tela de game over e restart aqui - PLAYER PERDEU
             }
-            else if (bossData.life < playerData.life)
-            {
-                lifeIcon[3].sprite = hollow;
-                RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Left);
-            }
-
+            else
+                RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Right);
+                return;
         }
         else if (bossData.life < playerData.life)                       //lembrando que preciso fazer isso pros 2 rounds(ganhos)
         {                                                               //daí vou precisar de uma bool nesses IF's
             lifeIcon[3].sprite = hollow;
-            RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Left);
+            
 
-            if ((bossData.life < playerData.life) && 
-                RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Right) < RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Left))//round1Ended == true)
+            if (RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Right) < RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Left))//round1Ended == true)
             {
                 lifeIcon[2].sprite = hollow;
                 RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Left);
+                return; //por enquanto!!!
                 //tela de vitória aqui
             }
-            else if (playerData.life < bossData.life)
-            {
-                lifeIcon[1].sprite = hollow;
-                RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Right);
-            }
+            else
+                RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Left);
+                return;
         }
     }
 
@@ -134,7 +143,7 @@ public class RoundCount : MonoBehaviour
             lifeIcon[0].sprite = hollow;
             
             
-            RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Right);
+            
             
             //escurecer a tela aqui e fazer a troca de rouds 
             if(RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Right) > RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Left)) //"se o boss tiver ganhado o primeiro round..."  - (round1Ended == true) Trocado pelo singleton
@@ -142,25 +151,41 @@ public class RoundCount : MonoBehaviour
                 RoundSingleton.Instance.FinalRound();
                 lifeIcon[1].sprite = hollow;
                 //tela de game over e restart aqui (lembrar de resetar a bool do round1
+            } 
+            
+            else
+            { RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Right); }
+
+            if(RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Right) < 2) 
+            {
+                playerData.life = 30f;
+                playerData.fainted = false; //pronto pra outro round >:3
             }
-            playerData.life = 200f;
-            playerData.fainted = false; //pronto pra outro round >:3
+            
         }
 
         else if (bossData.fainted == true)//(bossData.life <= 0)//&& round1Ended == false)                       //lembrando que preciso fazer isso pros 2 rounds(ganhos)
         {
             lifeIcon[3].sprite = hollow;
 
-            RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Left);
+            
 
             if (RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Left) > RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Left)) //"se o player tiver ganhado o primeiro round..."
             {
                 RoundSingleton.Instance.FinalRound();
                 lifeIcon[2].sprite = hollow;
                 //tela de vitória aqui
+            } 
+            
+            else
+            { RoundSingleton.Instance.RecordRoundWinner(winner: RoundSingleton.Side.Left); }
+
+            if(RoundSingleton.Instance.CurrentWinsOf(RoundSingleton.Side.Left) < 2)
+            {
+                bossData.life = 30f;
+                bossData.fainted = false;
             }
-            bossData.life = 200f;
-            bossData.fainted = false;
+                       
         }
     }
 
